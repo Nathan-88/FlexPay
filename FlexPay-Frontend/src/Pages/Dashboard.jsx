d5468bf5dd74ef443a849a3935b64f0568eaee24
samuelsuu <107080477+samuelsuu@users.noreply.github.com>
2024-02-08 10:43:45 +0100

    still working on the sidebar

---
+++ FlexPay-Frontend/src/Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import DashboardSidebar from "../components/Layout/DashboardLayout/components/DashboardSidebar"
+
+function Dashboard() {
+    return ( 
+        <div>
+            <DashboardSidebar />
+        </div>
+     );
+}
+
+export default Dashboard;
