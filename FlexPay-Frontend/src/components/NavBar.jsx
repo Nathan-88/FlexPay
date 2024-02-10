8afb126eacde5c01855c456bc4f7175ac4c1eadd
Nathan-88 <onwukaebuka88@gmail.com>
2024-02-10 10:17:04 +0100

    navbar

--- FlexPay-Frontend/src/components/NavBar.jsx
+++ FlexPay-Frontend/src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 import Logo from '../assets/images/Logo-removebg.png';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 const NavBar = () => {
     return (
         <nav className="flex justify-between items-center h-16 bg-white shadow">
-        <Link to="/">
+        <NavLink to="/">
             <img src={Logo} alt="logo" className="h-10 pl-4" />
-        </Link>
+        </NavLink>
         <div className="flex items-center pr-4 gap-x-10 font-bold">
-            <Link to="/login" className='text-blue-700 border-2 border-blue-700 rounded-3xl px-4 py-2'>Login</Link>
-            <Link to="/signup" className='text-white bg-blue-700 rounded-3xl px-4 py-2'>Signup</Link>
+            <NavLink to="/login" className='text-blue-700 border-2 border-blue-700 rounded-3xl px-4 py-2'>Login</NavLink>
+            <NavLink to="/signup" className='text-white bg-blue-700 rounded-3xl px-4 py-2'>Signup</NavLink>
         </div>
 
         </nav>
